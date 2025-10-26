37019481db406004ef2c50e3c2df255c89d121f4
itsmardochee <zossoungbomardoche@yahoo.fr>
2025-10-26 06:20:04 +0100

    chore(prettier): configure code formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    arrowParens: 'avoid',
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    endOfLine: 'lf',
+};
